ac75e8906ecc44d11e370495a49caa97f23d050b
Kamil Wrubliński <kamil.wrublinski@mirit.pl>
2026-05-12 23:03:19 +0200

    poprawki logowania

--- symfony-app/src/Controller/AuthController.php
+++ symfony-app/src/Controller/AuthController.php
@@ -4,38 +4,50 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Connection;
+use App\Form\LoginFormType;
+use App\Repository\AuthTokenRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends AbstractController
 {
-    #[Route('/auth/{username}/{token}', name: 'auth_login')]
-    public function login(string $username, string $token, Connection $connection, Request $request): Response
+    // TODO: zmiana z geta na post
+    #[Route('/auth', name: 'auth_login', methods: ['POST'])]
+    public function login(Request $request, FormFactoryInterface $formFactory, AuthTokenRepository $authTokenRepository, UserRepository $userRepository): Response
     {
-        $sql = "SELECT * FROM auth_tokens WHERE token = '$token'";
-        $result = $connection->executeQuery($sql);
-        $tokenData = $result->fetchAssociative();
+        $form = $formFactory->create(LoginFormType::class);
+        $form->submit($request->request->all());
 
-        if (!$tokenData) {
+        if (!$form->isValid()) {
+            $this->addFlash('error', 'You have to fill entire form!');
+            return $this->redirectToRoute('home');
+        }
+
+        $data = $form->getData();
+        $token = $data['token'];
+        $username = $data['username'];
+
+        $authToken = $authTokenRepository->findByToken($token);
+
+        if (!$authToken) {
             return new Response('Invalid token', 401);
         }
 
-        $userSql = "SELECT * FROM users WHERE username = '$username'";
-        $userResult = $connection->executeQuery($userSql);
-        $userData = $userResult->fetchAssociative();
+        $userData = $userRepository->findByUsername($username);
 
         if (!$userData) {
             return new Response('User not found', 404);
         }
 
         $session = $request->getSession();
-        $session->set('user_id', $userData['id']);
-        $session->set('username', $username);
+        $session->set('user_id', $userData->getId());
+        $session->set('username', $userData->getUsername());
 
-        $this->addFlash('success', 'Welcome back, ' . $username . '!');
+        $this->addFlash('success', 'Welcome back, ' . $userData->getUsername() . '!');
 
         return $this->redirectToRoute('home');
     }
---
+++ symfony-app/src/Form/LoginFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('token', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(message: 'Token is required'),
+                ],
+            ])
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new Assert\NotBlank(message: 'Username is required'),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
+    }
+}
