b42216d2d06b5ad3dc1c58745ffea6f338a47b13
Kamil Wrubliński <kamil.w.199865@gmail.com>
2026-05-14 06:36:03 +0200

    testy - osobna baza dla testów integracyjnych

--- symfony-app/config/bootstrap.php
+++ symfony-app/config/bootstrap.php
@@ -2,4 +2,14 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-// All environment variables are provided via Docker/system environment
+// Force test environment when running tests
+if (getenv('SYMFONY_ENV') === 'test' || getenv('APP_ENV') === 'test') {
+    $_SERVER['APP_ENV'] = 'test';
+    $_ENV['APP_ENV'] = 'test';
+    putenv('APP_ENV=test');
+    
+    // Override DATABASE_URL for test environment
+    $_SERVER['DATABASE_URL'] = 'postgres://postgres:postgres@symfony-db:5432/symfony_app_test';
+    $_ENV['DATABASE_URL'] = 'postgres://postgres:postgres@symfony-db:5432/symfony_app_test';
+    putenv('DATABASE_URL=postgres://postgres:postgres@symfony-db:5432/symfony_app_test');
+}
